371b6968277bfedf0665402f5b58e815a099316b
Михаил Драгункин <me@unsektor.com>
2020-01-15 01:32:45 +0300

    0.10.1 frontend.refactoring: bug fix for next years month rendering & major optimizations

--- public/assets/default/schedule-render.js
+++ public/assets/default/schedule-render.js
@@ -3,42 +3,38 @@ const ScheduleRender = function () {
         let transformed_data = {};
         for (let lesson of data) {
             if (undefined === transformed_data[lesson['teacher']]) {
-                transformed_data[lesson['teacher']] = {/* month */};
+                transformed_data[lesson['teacher']] = [];
             }
-
-            console.log(data)
 
             let lesson_date_start = new Date(lesson.interval[0].replace(' ', 'T'));
             let lesson_date_end = new Date(lesson.interval[1].replace(' ', 'T'));
 
+            let lesson_date = {
+                year: lesson_date_start.getFullYear(),
+                month: lesson_date_start.getMonth(),
+                day: lesson_date_start.getDate(),
+            };
 
-            let scalar_lesson_date_start =
-                lesson_date_start.getFullYear() + '-' +
-                (lesson_date_start.getMonth() + 1 + '').padStart(2, '0') + '-' +
-                (lesson_date_start.getDate() + '').padStart(2, '0');
-
-            let interval =
+            let lesson_interval =
                 (lesson_date_start.getHours() + '').padStart(2, '0') + ':' +
                 (lesson_date_start.getMinutes() + '').padStart(2, '0') + '-' +
                 (lesson_date_end.getHours() + '').padStart(2, '0') + ':' +
                 (lesson_date_end.getMinutes() + '').padStart(2, '0');
 
-            if (undefined === transformed_data[lesson['teacher']][scalar_lesson_date_start]) {
-                transformed_data[lesson['teacher']][scalar_lesson_date_start] = {};
-            }
-
-            transformed_data[lesson['teacher']][scalar_lesson_date_start][interval] = {
+            transformed_data[lesson['teacher']].push({
+                date: lesson_date,
+                interval: lesson_interval,
                 student: lesson.student,
                 type: lesson.type
-            };
+            });
         }
 
         return transformed_data
     }
 
-    function render_month(year, month, interval_list, data) {
+    function render_month(year, month, interval_list, lesson_map) {
         let month_days_count = (new Date(year, month, 0).getDate());  // https://stackoverflow.com/a/1184359
-        let html = `<h2>${year}-${month}</h2>`;
+        let html = `<h2>${year}-${(month+1).toString().padStart(2, '0')}</h2>`;
 
         // <table>
         html += '<table class="schedule__month">';
@@ -57,19 +53,13 @@ const ScheduleRender = function () {
             html += '<tr>';
             html += `<td>${interval}</td>`;
 
-            for (let i = 1; i <= month_days_count; i++) {
-                let index = year + '-' + month.toString().padStart(2, '0') + '-' + i.toString().padStart(2, '0');
-
+            for (let day = 1; day <= month_days_count; day++) {
                 let value = '';
                 let style = '';
 
-                console.log(data);
-                if (index in data) {
-                    // value = interval
-                    if (interval in data[index]) {
-                        value = data[index][interval].student;
-                        style = (data[index][interval].type === 0) ? 'style="background: #ddd"' : '';
-                    }
+                if (day+interval in lesson_map) {
+                    value = lesson_map[day+interval].student;
+                    style = (lesson_map[day+interval].type === 0) ? 'style="background: #ddd"' : '';
                 }
 
                 html += `<td ${style}>${value}</td>`
@@ -86,14 +76,24 @@ const ScheduleRender = function () {
     }
 
     function render_teacher(teacher, teacher_data, metadata) {
-        let month_set = new Set();  // 1,70 * 75
-        for (let lesson_date of Object.keys(teacher_data)) {
-            month_set.add(new Date(lesson_date).getMonth() + 1);
+        let month_set = {};
+        for (let lesson of teacher_data) {
+            let key = lesson.date.year+'-'+lesson.date.month.toString().padStart(2, '0');
+
+            if (undefined === month_set[key]) {
+                month_set[key] = {
+                    year: lesson.date.year,
+                    month: lesson.date.month,
+                    lessons: {}
+                };
+            }
+
+            month_set[key].lessons[lesson.date.day + lesson.interval] = lesson
         }
 
         let html = `<h1>Преподавтель #${teacher}</h1>`;
-        for (let month of month_set.values()) {
-            html += render_month(2019, month, metadata, teacher_data);
+        for (let key of Object.keys(month_set).sort()) {
+            html += render_month(month_set[key].year, month_set[key].month, metadata, month_set[key].lessons);
         }
 
         return html;
